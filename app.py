87e24502446350b67d7216b19989a34bce5e4e0d
Reconstructive Technologies <rballe33@hotmail.com>
2021-02-13 09:24:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def predict():
 
 
 if __name__ == '__main__':
-    # app.run(port=5002, threaded=False)
+    app.run(debug=False)
 
     # Serve the app with gevent
     http_server = WSGIServer(('0.0.0.0', 5000), app)
