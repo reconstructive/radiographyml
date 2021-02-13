a54aa8ae4cec98af5735c72929009401e3a799e0
Reconstructive Technologies <rballe33@hotmail.com>
2021-02-13 10:34:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ print("path", THIS_FOLDER)
 print(os.listdir())
 print(os.listdir('/app/models/'))
 #MODEL_PATH = '/app/models/'
-MODEL_PATH = '/app/models/your_model.h5'
+MODEL_PATH = '/app/models/my_model.h5'
 
 # Load your own trained model
 model = load_model(MODEL_PATH)
