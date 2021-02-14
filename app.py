2e64c08932ed57a240115234d5fd82d28a706f2f
Reconstructive Technologies <rballe33@hotmail.com>
2021-02-14 11:40:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ MODEL_PATH = 'models/'
 
 # Load your own trained model
 #model = load_model(MODEL_PATH)
-model = tf.saved_model.load('my_model')
+model = tf.saved_model.load('models')
 #model = load_model(os.path.join(os.getcwd(), 'models', 'my_model.h5'))
 #model = load_model(os.path.join('models', 'my_model.h5'))
 #model = model.model()
